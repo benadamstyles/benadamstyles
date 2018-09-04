733b97b9fe1cfe681cc2775fb50522f7b06ebc81
Ben Styles <enquiries@leeds-ebooks.co.uk>
2018-09-04 21:30:15 +0100

    Link sources to npmjs.com

--- src/components/sources.js
+++ src/components/sources.js
@@ -9,18 +9,21 @@ const center = css`
   text-align: center;
 `
 
+const getHtml = () => ({
+  __html: Seq(pkg.dependencies)
+    .keySeq()
+    .filterNot(dep => dep.includes('plugin'))
+    .map(dep => `<a href="https://www.npmjs.com/package/${dep}">${dep}</a>`)
+    .reduce(
+      (out, dep, i, seq) =>
+        i === seq.count() - 1 ? `${out} & ${dep}` : `${out}, ${dep}`
+    ),
+})
+
 export const Sources = () => (
   <div>
     <p className={center}>Brought to you with</p>
-    <p className={center}>
-      {Seq(pkg.dependencies)
-        .keySeq()
-        .filterNot(k => k.includes('plugin'))
-        .reduce(
-          (out, k, i, seq) =>
-            i === seq.count() - 1 ? `${out} & ${k}` : `${out}, ${k}`
-        )}
-    </p>
+    <p className={center} dangerouslySetInnerHTML={getHtml()} />
     <p className={center}>from Leeds, Yorkshire, UK</p>
   </div>
 )
