52206c0e97259e76f5fcb07eab3d6c16f387646a
Benjamin Styles <benjamin.styles@dazn.com>
2020-04-26 16:59:56 +0100

    Better loading

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -4,25 +4,58 @@ import { highlightColor } from '../css/colors'
 
 const rotate = keyframes`
   to {
-    transform: translate(-50%, -50%) rotate(360deg);
+    transform: rotate(360deg);
   }
 `
 
+const DURATION = 0.8
+
 const style = css`
   position: fixed;
-  top: 50vh;
-  left: 50vw;
-  width: 3px;
+  width: 10px;
   height: 100px;
 
-  border-top: 3px solid ${highlightColor};
-  filter: blur(0.2px);
-
+  top: 50vh;
+  left: 50vw;
   transform: translate(-50%, -50%);
-  animation: ${rotate} 1.25s infinite;
-  transition: transform 0.5s linear;
+
+  opacity: 0.5;
+
+  > div {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+
+    animation: ${rotate} ${DURATION}s infinite;
+
+    :nth-of-type(2) {
+      animation-delay: ${DURATION * 0.18}s;
+    }
+    :nth-of-type(3) {
+      animation-delay: ${DURATION * 0.36}s;
+    }
+
+    > div {
+      width: 10px;
+      height: 10px;
+      background-color: ${highlightColor};
+      border-radius: 50%;
+    }
+  }
 `
 
-const Loading = () => <div css={style} role="progressbar" />
+const Loading = () => (
+  <div css={style} role="progressbar">
+    <div>
+      <div />
+    </div>
+    <div>
+      <div />
+    </div>
+    <div>
+      <div />
+    </div>
+  </div>
+)
 
 export default Loading
