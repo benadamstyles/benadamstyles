814b64b7af2eee2f47f2d194a3fdd69219431f41
Benjamin Styles <benjamin.styles@dazn.com>
2020-05-04 12:17:48 +0100

    Change some styles to objects for type checking

--- src/components/navigation/nav-link.tsx
+++ src/components/navigation/nav-link.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react'
 import { useStaticInfo } from 'react-static'
-import { css } from '@emotion/core'
 import { linkIsActive } from '../../util/path'
 import { highlightColor, textColor } from '../../css/colors'
 
-const style = css`
-  color: ${textColor};
-`
+const style = {
+  color: textColor,
+}
 
-const activeStyle = css`
-  color: ${highlightColor};
-`
+const activeStyle = {
+  color: highlightColor,
+}
 
 const NavLink: React.FC<Require<
   React.AnchorHTMLAttributes<HTMLAnchorElement>,
