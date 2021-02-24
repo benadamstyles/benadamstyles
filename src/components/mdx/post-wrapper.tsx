898311c8cc0c6f0d9d4e7b8b63a9eaab9a3a5486
Ben Styles <enquiries@leeds-ebooks.co.uk>
2021-02-24 22:29:40 +0000

    Add tag filter

--- src/components/mdx/post-wrapper.tsx
+++ src/components/mdx/post-wrapper.tsx
@@ -1,42 +1,7 @@
 import React from 'react'
-import styled from '@emotion/styled'
 
 import type { BlogPost } from '../../pages/blog'
-import { phone, smallPhone } from '../../css/Breakpoints.gen'
-import { hintColor } from '../../css/colors'
-
-const Main = styled.main`
-  padding: 5rem;
-  margin: 0 auto;
-  max-width: calc(70ch + 10rem);
-
-  @media (max-width: ${phone}) {
-    padding: 2rem;
-  }
-
-  @media (max-width: ${smallPhone}) {
-    padding: 2rem 1rem;
-  }
-
-  /* NOTE: typography */
-  line-height: 1.5;
-  font-kerning: normal;
-  letter-spacing: 0.03rem;
-
-  p + p {
-    margin-top: 1rem;
-  }
-
-  nav {
-    background-color: ${hintColor};
-    padding: 0.2rem;
-    margin: 2rem 0;
-
-    ol {
-      list-style-type: '–  ';
-    }
-  }
-`
+import { Main } from '../layout/main'
 
 interface PostProps extends Omit<BlogPost, 'slug'> {
   is404: boolean
