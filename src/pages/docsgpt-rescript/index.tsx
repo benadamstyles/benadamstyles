0733472e3afc6e5b26f9a4a0aefcf88fa3c546ef
Ben Styles <benadamstyles@gmail.com>
2023-03-11 23:00:31 +0100

    Form improvements

--- src/pages/docsgpt-rescript/index.tsx
+++ src/pages/docsgpt-rescript/index.tsx
@@ -12,6 +12,12 @@ const Button = styled.button({
   },
 })
 
+const Form = styled.form({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+})
+
 interface Question {
   question: string
   apiKey: string
@@ -46,38 +52,70 @@ async function submitQuestion({ question, apiKey, apiHost }: Question) {
   return answer
 }
 
+interface Query<Data> {
+  data: Data
+  loading: boolean
+  error: Error | undefined
+}
+
 const DocsGptRescript = () => {
   const [apiKey, setApiKey] = React.useState('')
   const [apiHost, setApiHost] = React.useState('')
   const [question, setQuestion] = React.useState('')
-  const [answer, setAnswer] = React.useState('')
+
+  const [{ data, loading, error }, setQuery] = React.useState<Query<string>>({
+    data: '',
+    loading: false,
+    error: undefined,
+  })
 
   return (
     <Main>
-      <input
-        placeholder="OpenAI API key"
-        value={apiKey}
-        onChange={event => setApiKey(event.target.value)}
-      />
-      <input
-        placeholder="API host"
-        value={apiHost}
-        onChange={event => setApiHost(event.target.value)}
-      />
-      <textarea
-        placeholder="Question"
-        value={question}
-        onChange={event => setQuestion(event.target.value)}
-      />
-      <Button
-        type="button"
-        onClick={() =>
-          submitQuestion({ question, apiKey, apiHost }).then(setAnswer)
-        }>
-        Try me!
-      </Button>
+      <Form
+        onSubmit={event => {
+          event.preventDefault()
+          setQuery(prev => ({ ...prev, loading: true }))
+          submitQuestion({ question, apiKey, apiHost })
+            .then(answer =>
+              setQuery(prev => ({ ...prev, loading: false, data: answer }))
+            )
+            .catch((err: Error) =>
+              setQuery(prev => ({ ...prev, loading: false, error: err }))
+            )
+        }}>
+        <div>
+          <input
+            placeholder="OpenAI API key"
+            value={apiKey}
+            onChange={event => setApiKey(event.target.value)}
+          />
+          <input
+            placeholder="API host"
+            value={apiHost}
+            onChange={event => setApiHost(event.target.value)}
+          />
+        </div>
+        <textarea
+          placeholder="Question"
+          value={question}
+          onChange={event => setQuestion(event.target.value)}
+          onKeyPress={event => {
+            if (event.key === 'Enter' && !event.shiftKey) {
+              event.preventDefault()
+              const { form } = event.target as HTMLTextAreaElement
+
+              if (form) {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call -- requestSubmit() is a standard DOM API.
+                form.requestSubmit()
+              }
+            }
+          }}
+        />
+        <Button type="submit">Try me!</Button>
+      </Form>
       <p>
-        <strong>Answer:</strong> {answer}
+        <strong>Answer:</strong>{' '}
+        {loading ? '...loading...' : error?.toString() || data}
       </p>
     </Main>
   )
